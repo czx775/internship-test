46bd86c31c459b70b288d624884744c49eab2c5c
Nicolas Jonnathan <73278254+czx775@users.noreply.github.com>
2023-03-02 14:22:19 +0700

    added update/delete function

--- simpleProject/controllers/controllers.go
+++ simpleProject/controllers/controllers.go
@@ -43,14 +43,49 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetUserById(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	id := r.FormValue("id")
 
+	db := databases.ConnectDatabase()
+	defer db.Close()
+
+	row, err := db.Query("SELECT id, name, age FROM user WHERE id = ?", id)
+	if err != nil {
+		log.Print(err)
+	}
+
+	for row.Next() {
+		err := row.Scan(&user.ID, &user.Name, &user.Age)
+		if err != nil {
+			log.Print(err)
+		}
+	}
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	db := databases.ConnectDatabase()
+	defer db.Close()
 
+	id := r.FormValue("id")
+	name := r.FormValue("name")
+	age := r.FormValue("age")
+
+	_, err := db.Exec("UPDATE user SET name=?, age=? WHERE id=?", name, age, id)
+	if err != nil {
+		log.Print(err)
+	}
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	db := databases.ConnectDatabase()
+	defer db.Close()
+
+	id := r.FormValue("id")
+
+	_, err := db.Exec("DELETE FROM user WHERE id=?", id)
+	if err != nil {
+		log.Print(err)
+	}
 
 }
 
--- simpleProject/databases/mysql.go
+++ simpleProject/databases/mysql.go
@@ -3,12 +3,13 @@ package databases
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
 func ConnectDatabase() *sql.DB {
 	db, err := sql.Open("mysql", "root:nicolas1@tcp(127.0.0.1:3306)/simpleProject")
 	if err != nil {
-		panic(err.Error())
+		log.Print(err)
 	}
 	return db
 }
